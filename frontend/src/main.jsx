cc6e182eb5343519169dad5bb56cb02b5155bf13
jeico <jeicomorales1@gmail.com>
2025-10-18 09:46:51 -0600

    Fix SPA routing with BrowserRouter to prevent 404 in Vercel

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,12 +2,15 @@ import './styles/index.css';
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App.jsx';
+import { BrowserRouter } from 'react-router-dom';
 import { GoogleOAuthProvider } from '@react-oauth/google';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <GoogleOAuthProvider clientId="661455241156-d7m8033gbkemga4ihse0qnjgea4i602e.apps.googleusercontent.com">
-      <App />
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
     </GoogleOAuthProvider>
   </StrictMode>
 );
